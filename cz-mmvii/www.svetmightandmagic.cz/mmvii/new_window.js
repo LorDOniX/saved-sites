5ef6432002dafab880648a1c7602e6b5cac93c6c
Roman Makudera <rmakudera@gmail.com>
2022-10-24 08:58:35 +0200

    init;

---
+++ cz-mmvii/www.svetmightandmagic.cz/mmvii/new_window.js
@@ -0,0 +1,3 @@
+function nove_okno(soubor, jmeno, sirka, delka) {
+   var okno = window.open(soubor, jmeno, "width="+sirka+",height="+delka+",status=yes,resizable=yes,scrollbars=yes,left=30,top=30");
+}
